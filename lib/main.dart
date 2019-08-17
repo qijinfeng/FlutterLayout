723afc86e8e8e52da7af4011917535fb8b701be2
qijinfeng <qijinfeng@kp99.cn>
2019-08-17 16:26:42 +0800

    widget状态管理

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(new MyApp());
@@ -6,7 +7,8 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     Widget titleSection = new Container(
-      padding: const EdgeInsets.only(top:32.0,left: 15,right: 15,bottom: 15),
+      padding: const EdgeInsets.only(
+          top: 32.0, left: 15, right: 15, bottom: 15),
       child: new Row(
         children: [
           new Expanded(
@@ -18,7 +20,7 @@ class MyApp extends StatelessWidget {
                   child: new Text(
                     'Oeschinen Lake Campground dahjsg d a adjskha ajkdha akjdha akdsha akdsjha askdja dalisdj adkjal ',
                     maxLines: 2,
-                    overflow: TextOverflow.clip,
+                    overflow: TextOverflow.ellipsis,
                     style: new TextStyle(
                       fontWeight: FontWeight.bold,
                     ),
@@ -52,11 +54,13 @@ class MyApp extends StatelessWidget {
     );
 
     Widget textSection = new Container(
-      padding: const EdgeInsets.all(32.0),
+      padding: const EdgeInsets.only(top: 32.0, left: 15.0, right: 15.0),
       child: new Text(
-        '''
-Lake Oeschinen lies at the foot of the Blüemlisalp in the Bernese Alps. Situated 1,578 meters above sea level, it is one of the larger Alpine Lakes. A gondola ride from Kandersteg, followed by a half-hour walk through pastures and pine forest, leads you to the lake, which warms to 20 degrees Celsius in the summer. Activities enjoyed here include rowing, and riding the summer toboggan run.
-        ''',
+        '''Lake Oeschinen lies at the foot of the Blüemlisalp in the Bernese Alps. 
+        Situated 1,578 meters above sea level,it is one of the larger Alpine Lakes.
+         A gondola ride from Kandersteg, followed by a half-hour walk through pastures and pine forest,
+         leads you to the lake,which warms to 20 degrees Celsius in the summer.
+         Activities enjoyed here include rowing, and riding the summer toboggan run.''',
         softWrap: true,
       ),
     );
@@ -78,6 +82,8 @@ Lake Oeschinen lies at the foot of the Blüemlisalp in the Bernese Alps. Situate
             titleSection,
             buttonSection,
             textSection,
+            new TapBoxA(),
+            new ParentWidget(),
           ],
         ),
       ),
@@ -85,7 +91,9 @@ Lake Oeschinen lies at the foot of the Blüemlisalp in the Bernese Alps. Situate
   }
 
   Column buildButtonColumn(BuildContext context, IconData icon, String label) {
-    Color color = Theme.of(context).primaryColor;
+    Color color = Theme
+        .of(context)
+        .primaryColor;
     return new Column(
       mainAxisSize: MainAxisSize.min,
       mainAxisAlignment: MainAxisAlignment.center,
@@ -107,6 +115,7 @@ Lake Oeschinen lies at the foot of the Blüemlisalp in the Bernese Alps. Situate
   }
 }
 
+///有状态的widget,点击改变数量
 class FavoriteWidget extends StatefulWidget {
   @override
   State<StatefulWidget> createState() {
@@ -152,7 +161,8 @@ class _FavoriteWidgetState extends State<FavoriteWidget> {
           child: new Container(
             child: new Text(
               '$_favoriteCount',
-              softWrap: true,
+              softWrap: false,
+              overflow: TextOverflow.ellipsis,
               style: new TextStyle(
                   fontStyle: FontStyle.normal, fontWeight: FontWeight.w700),
             ),
@@ -160,5 +170,139 @@ class _FavoriteWidgetState extends State<FavoriteWidget> {
         ),
       ],
     );
+  }
+}
+
+// TapBoxA 管理自身状态.
+
+//------------------------- TapBoxA ----------------------------------
+class TapBoxA extends StatefulWidget {
+  TapBoxA({Key key}) : super(key: key);
+
+  @override
+  _TapBoxAState createState() => new _TapBoxAState();
+}
+
+
+class _TapBoxAState extends State<TapBoxA> {
+
+  bool _active = false;
+
+  void _handleTap() {
+    setState(() {
+      _active = !_active;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return new GestureDetector(
+      onTap: _handleTap, //点击回调
+      child: new Container(
+        child: new Center(
+            child: new Text(
+              _active ? 'Active' : 'Inactive',
+              style: new TextStyle(fontSize: 32.0, color: Colors.white),
+            )),
+        width: 200,
+        height: 200,
+        decoration: new BoxDecoration(
+            color: _active ? Colors.lightGreen[700] : Colors.grey[600]
+        ),
+      ),
+    );
+  }
+}
+
+// ParentWidget 为 TapBoxB 管理状态.
+
+//------------------------ ParentWidget --------------------------------
+class ParentWidget extends StatefulWidget {
+  @override
+  _ParentWidgetState createState() => new _ParentWidgetState();
+}
+
+class _ParentWidgetState extends State<ParentWidget> {
+  bool _active = false;
+
+  void _handleTapBoxChanged(bool newValue) {
+    setState(() {
+      _active = newValue;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return new Container(
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: <Widget>[
+          new TapBoxB(
+            active: _active,
+            onChanged: _handleTapBoxChanged,
+          ),
+          new TapBoxC(
+              active: _active,
+              onChanged: _handleTapBoxChanged)
+        ],
+      ),
+
+    );
+  }
+}
+
+//------------------------- TapBoxB ----------------------------------
+
+class TapBoxB extends StatelessWidget {
+  //@required必填参数，要使用@required注解，需要导入foundation library（该库重新导出Dart的meta.dart）
+  // 'package: flutter/foundation.dart';
+  TapBoxB({Key key, this.active: false, @required this.onChanged})
+      : super(key: key);
+
+  final bool active;
+  final ValueChanged<bool> onChanged; //一个方法
+
+  void _handleTap() {
+    onChanged(!active);
+  }
+
+  Widget build(BuildContext context) {
+    return new GestureDetector(
+      onTap: _handleTap,
+      child: new Container(
+        child: new Center(
+          child: new Text(
+            active ? 'Active' : 'Inactive',
+            style: new TextStyle(fontSize: 32.0, color: Colors.white),
+          ),
+        ),
+        width: 200.0,
+        height: 200.0,
+        decoration: new BoxDecoration(
+          color: active ? Colors.lightGreen[700] : Colors.grey[600],
+        ),
+      ),
+    );
+  }
+}
+
+
+//----------------------------- TapBoxC ------------------------------
+class TapBoxC extends StatefulWidget {
+
+  TapBoxC({Key key, this.active: false, @required this.onChanged})
+      :super(key: key);
+
+  final bool active;
+  final ValueChanged<bool> onChanged;
+
+  @override
+  _TapBoxCState createState() => new _TapBoxCState();
+}
+
+class _TapBoxCState extends State<TapBoxC> {
+  @override
+  Widget build(BuildContext context) {
+    return null;
   }
 }
